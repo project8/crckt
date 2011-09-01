d4274961563ce134af0bcb83ccbad827c94cb6d1
Jared Kofron <jared.kofron@gmail.com>
2011-09-01 16:08:02 -0700

    work in progress.  currently just dumps exponential samples to a file.

--- src/crckt.c
+++ src/crckt.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+#include "crckt_opts.h"
 #include "crckt_exp.h"
 
-int main() {
+int main(int argc, char** argv) {
+  int result = crckt_normal;
 
-  mt_init();
+  // Options for the run
+  crckt_run_info* opts = malloc(sizeof(crckt_run_info));
 
-  FILE* out = fopen("output.dat","w");
+  if( args_to_info(opts,argc,argv) ) {
+    // Initialize the random number generator
+    mt_init();
+    FILE* out = fopen("output.dat","w");
 
-  unsigned long int sample_rate = 100;
-  unsigned long int event_rate = 1000;
+    unsigned long int sample_rate = 100;
+    unsigned long int event_rate = 1000;
 
-  unsigned long int sample;
-  for(int i = 0; i < 10000; i++) {
-    sample = crckt_exp_draw(sample_rate,event_rate);
-    fprintf(out,"%lu\n",sample);
+    unsigned long int sample;
+    for(int i = 0; i < 10000; i++) {
+      sample = crckt_exp_draw(sample_rate,event_rate);
+      fprintf(out,"%lu\n",sample);
+    }
+
+    fclose(out);
   }
 
-  fclose(out);
-  return 0;
+  else {
+    result = crckt_error;
+  }
+
+  return result;
 }
