9bb4cbe45b29f8eb1c7d7f0c2d4d007ea6503610
Jared Kofron <jared.kofron@gmail.com>
2011-09-02 11:10:35 -0700

    check status of args_to_opts

--- src/crckt.c
+++ src/crckt.c
@@ -2,13 +2,16 @@
 #include "crckt_opts.h"
 #include "crckt_exp.h"
 
+// The size of the data records that mantis generates
+const static int mantis_sample_size = 4194304;
+
 int main(int argc, char** argv) {
   int result = crckt_normal;
 
   // Options for the run
   crckt_run_info* opts = malloc(sizeof(crckt_run_info));
 
-  if( args_to_info(opts,argc,argv) ) {
+  if( args_to_info(opts,argc,argv) != -1 ) {
     // Print information to terminal
     print_run_info(opts);
 
@@ -18,11 +21,11 @@ int main(int argc, char** argv) {
 
     unsigned long int sample_rate = 100;
     unsigned long int event_rate = 1000;
+    unsigned long int sample_count = 0;
 
-    unsigned long int sample;
-    for(int i = 0; i < 10000; i++) {
-      sample = crckt_exp_draw(sample_rate,event_rate);
-      fprintf(out,"%lu\n",sample);
+    while( sample_count < mantis_sample_size ) {
+      sample_count += crckt_exp_draw(sample_rate,event_rate);
+      fprintf(out,"%lu\n",sample_count);
     }
 
     fclose(out);
