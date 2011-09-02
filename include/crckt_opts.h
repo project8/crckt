83f3783620b66518f428b90c3f7c10cbc17a5ddd
Jared Kofron <jared.kofron@gmail.com>
2011-09-01 20:32:24 -0700

    added configuration parameter bitmasks

--- include/crckt_opts.h
+++ include/crckt_opts.h
@@ -8,6 +8,16 @@
 const static int crckt_normal = 0;
 const static int crckt_error  = 1;
 
+/* bitmask constants for configuration */
+
+#define chk_bit(someinfo,somebit) ((someinfo->conf_mask & somebit) == somebit)
+const static int event_rate_set = 0x1;
+const static int sample_rate_set = 0x2;
+const static int f_center_set = 0x4;
+const static int f_bw_set = 0x6;
+const static int sn_ratio_set = 0x8;
+const static int e_tof_set = 0x10;
+
 /*
  * i like structs.  they help keep things clean.  this one contains 
  * all of the information regarding a particular cricket run.
@@ -19,6 +29,7 @@ typedef struct {
   unsigned long int f_bw;     // Chirp bandwidth, Hz
   unsigned long int e_tof;    // time of flight in seconds
   unsigned int sn_ratio;      // A constant
+  unsigned char conf_mask;    // Configuration info about user settings
 } crckt_run_info;
 
 /*
@@ -26,5 +37,11 @@ typedef struct {
  * calls getopt and does some magical things.
  */
 int args_to_info(crckt_run_info* tgt, int argc, char** argv);
+
+
+/*
+ * takes care of default parameters
+ */
+void process_defaults(crckt_run_info* info);
 
 #endif // __crckt_opts_h
